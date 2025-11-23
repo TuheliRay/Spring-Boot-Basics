e3dba3e46aab338c3721fb84d8737eb49f6953aa
Tuheli Ray <tuheliray@gmail.com>
2025-11-23 15:21:25 +0530

    Added a file for storing constants

---
+++ src/main/java/com/SpringBoot/CRUD/Constants/APIpaths.java
@@ -0,0 +1,9 @@
+package com.SpringBoot.CRUD.Constants;
+
+public class APIpaths {
+    public static final String STUDENT = "/student";
+    public static final String ADD = "/add";
+    public static final String GET = "/{id}";
+    public static final String UPDATE = "/update/{id}";
+    public static final String DELETE = "/delete/{id}";
+}
--- src/main/java/com/SpringBoot/CRUD/controllers/StudentController.java
+++ src/main/java/com/SpringBoot/CRUD/controllers/StudentController.java
@@ -1,31 +1,31 @@
 package com.SpringBoot.CRUD.controllers;
 
+import com.SpringBoot.CRUD.Constants.APIpaths;
 import com.SpringBoot.CRUD.entities.Student;
 import com.SpringBoot.CRUD.services.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 @AllArgsConstructor
-@RequestMapping("/student")
+@RequestMapping(APIpaths.STUDENT)
 @RestController
 public class StudentController {
     private final StudentService studentService;
-    @PostMapping("/add")
+    @PostMapping(APIpaths.ADD)
     public Student addStudent(@RequestBody Student student) {
         return studentService.addStudent(student);
     }
-    @GetMapping("/{id}")
+    @GetMapping(APIpaths.GET)
     public Student getStudentById(@PathVariable Integer id) {
         return studentService.findStudentById(id);
     }
     public Student findAllStudentsById(@PathVariable Integer id) {
         return studentService.findStudentById(id);
     }
-    @PutMapping("/update/{id}")
+    @PutMapping(APIpaths.UPDATE)
     public Student updateStudentById(@RequestBody Student student , @PathVariable Integer id) {
         return studentService.updateStudent(student , id);
     }
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping(APIpaths.DELETE)
     public void deleteStudentById(@PathVariable Integer id) {
         studentService.deleteStudentById(id);
     }
